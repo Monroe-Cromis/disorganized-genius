d74fde3591da5fc1f1efd03c5a29e3aef2bfe23d
Monroe Cromis <monroecromis@Marleys-iMac.local>
2022-04-07 23:56:17 -0700

    uploading files

--- script.js
+++ script.js
@@ -8,13 +8,78 @@ var time= moment().format("MMM Do, YYYY HH: mm: ss: a");
 document.querySelector("#time").textContent= time;
 },1000)
 
+
+//saving to local storage 
+
+var entry = document.querySelector("#entry");
+var save = document.querySelector('#Save');
+
+
+var task = localStorage.setItem("task")
+entry.textContent = task; 
+
+saveBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  .onClick ("clicked", "#entry")
+    localStorage.setItem("#entry")
+    localStorage.getItem("#entry")
+
+  saveBtn.addEventListener("click")
+  }
+  task.textContent.setItem("task", task)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+/*const entryTask = document.querySelectorAll('entry')
+
+saveBtn
+
+let saveArr = JSON.parse(localStorage.getItem('savArr')) || []
+saveBtn.forEach(function(saves){
+    saves.addEventListener("click", function (event)}
+    let row = event.target.parentNOde 
+    let myContent= row.querySelector)'description').value;
+    let hour = row.querySelector ('hour").textContent
+    saveArr.push({
+        myContenthour
+    }}
+    
+    AudioD'
+})
+
+
+saveBtn.addEventListener("click")
+
+var saveBtn = document.querySelectorAll("#save")
+if function("clicked"){
+localStorage.setItem("#entry")
+localStorage.getItem("#entry")
+
+})
+
+
+
+
+
+
+
 //color blocks--------------------------------------------//
 
 
-/*//const colorcode = document.getElementsByClassName("colorcode");//<- switched to get elements with class corresponding to the given html
-let currentHour = Date.now().getHours();//get current time hour
+/*//*const colorcode = document.getElementsByClassName("colorcode");//<- switched to get elements with class corresponding to the given html
+/*let currentHour = Date.now().getHours();//get current time hour
 
-colorcode.forEach(function(colorcodedDiv){//just a syntax I use
+/*colorcode.forEach(function(colorcodedDiv){//just a syntax I use
     let colorcodeHour = parseInt(colorcodedDiv.id);
   if (colorcodeHour) {
     if (currentHour === colorcodeHour) {
@@ -31,17 +96,6 @@ colorcode.forEach(function(colorcodedDiv){//just a syntax I use
 function setColor(element, color) {
     element.style.backgroundColor = color;
 }//added curly bracket since it was missing */
-
-
-// Local Storage function
-
-var saveEl = document.querySelector("#save");
-
-function showResponse(event){
-    event.preventDefault();
-    console.log(event);
-
-}
 
 
 
